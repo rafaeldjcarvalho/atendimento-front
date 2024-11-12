0285454634c8c73ec22aa444742d5a4429da476f
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2024-11-12 16:11:27 -0300

    feat: excluido imports

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -3,13 +3,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
   imports: [
-    RouterOutlet,
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
